ea5e378beae8f6a8810fac2088831c332ef3ce79
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-09-20 17:33:01 -0400

    adding the auction model

---
+++ models/Auction.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Models;
+
+use App\Models\DB\CRUD;
+use App\Models\User;
+
+class Auction extends CRUD {
+    protected $table = 'auctions';
+
+    public function findByStampId($stampId) {
+        $sql = "SELECT * FROM " . $this->table . " WHERE stamp_id = :stamp_id AND start_date <= CURDATE() AND end_date >= CURDATE()";
+        $stmt = $this->prepare($sql);
+        $stmt->bindParam(':stamp_id', $stampId);
+        $stmt->execute();
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+}
